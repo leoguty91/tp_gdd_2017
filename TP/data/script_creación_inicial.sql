5ff555080380d4c5d24d2eee83c4fd983acc1562
leoguty91 <leoguty91@gmail.com>
2017-05-16 12:17:48 -0300

    Alta usuario
    Se agrega el SP alta_usuario

--- TP/data/script_creación_inicial.sql
+++ TP/data/script_creación_inicial.sql
@@ -10,11 +10,14 @@ IF EXISTS (SELECT NAME FROM sys.indexes WHERE NAME = N'IX_Chofer')
 GO
 
 /* Eliminacion de Triggers */
-/*
 IF OBJECT_ID ('GGDP.T_Cliente_telefono_unico','TR') IS NOT NULL
    DROP TRIGGER GGDP.T_Cliente_telefono_unico;
 GO
-*/
+
+/* Eliminacion de Store Procedures */
+IF OBJECT_ID ('GGDP.alta_cliente') IS NOT NULL
+    DROP PROCEDURE GGDP.alta_cliente
+GO
 
 /* Eliminacion de Tablas */
 IF OBJECT_ID('GGDP.Cliente') IS NOT NULL
@@ -96,5 +99,45 @@ BEGIN
 	ROLLBACK TRANSACTION;
 	RETURN;
 END;
+GO
+*/
+
+/* Creacion de Store Procedures */
+/*
+CREATE PROCEDURE GGDP.alta_cliente
+(
+	@usuario VARCHAR(255),
+	@password VARCHAR(255),
+	@rol INT,
+	@nombre VARCHAR(255),
+	@apellido VARCHAR(255),
+	@dni NUMERIC(18, 0),
+	@mail VARCHAR(255),
+	@telefono NUMERIC(18, 0),
+	@direccion VARCHAR(255),
+	@codigo_postal VARCHAR(8),
+	@fecha_nacimiento DATETIME
+) AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+		DECLARE @codigo_usuario INT
+		/* TODO FALTA EL SP ALTA USUARIO */
+		EXEC @codigo_usuario = GGDP.alta_usuario(@usuario, @password, @rol)
+
+		IF @codigo_usuario = -1
+			RAISERROR('El usuario ya existe', 16, 1)
+
+		INSERT INTO GGDP.Cliente(clie_nombre, clie_apellido, clie_dni, clie_mail, clie_telefono, clie_direccion, clie_codigo_postal, clie_fecha_nacimiento, clie_habilitado, clie_usuario)
+		VALUES(@nombre, @apellido, @dni, @mail, @telefono, @direccion, @codigo_postal, @fecha_nacimiento, 1, @codigo_usuario)
+
+		COMMIT TRANSACTION
+			SELECT @codigo_usuario
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+			SELECT -1
+	END CATCH
+END
 GO
 */
