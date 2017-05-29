35e6d8efdf1c7ba4c5bc3c667fd667ff575593fa
leoguty91 <leoguty91@gmail.com>
2017-05-28 22:25:01 -0300

    Correciones varias
    Se corrigieron varias tablas, ahora funciona todo al re-correr toda la
    consulta

--- TP/data/script_creación_inicial.sql
+++ TP/data/script_creación_inicial.sql
@@ -30,7 +30,67 @@ IF OBJECT_ID ('GGDP.modificacion_cliente') IS NOT NULL
     DROP PROCEDURE GGDP.modificacion_cliente
 GO
 
+/* Eliminacion de los FK */
+/*
+IF OBJECT_ID ('fk_auto_chofer', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Automovil DROP CONSTRAINT fk_auto_chofer;
+
+IF OBJECT_ID ('fk_viaj_chofer', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Viaje DROP CONSTRAINT fk_viaj_chofer;
+IF OBJECT_ID ('fk_viaj_automovil', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Viaje DROP CONSTRAINT fk_viaj_automovil;
+IF OBJECT_ID ('fk_viaj_turno', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Viaje DROP CONSTRAINT fk_viaj_turno;
+IF OBJECT_ID ('fk_viaj_cliente', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Viaje DROP CONSTRAINT fk_viaj_cliente;
+
+IF OBJECT_ID ('fk_rend_chofer', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Rendicion DROP CONSTRAINT fk_rend_chofer;
+IF OBJECT_ID ('fk_rend_turno', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Rendicion DROP CONSTRAINT fk_rend_turno;
+IF OBJECT_ID ('fk_rend_viaje', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Rendicion DROP CONSTRAINT fk_rend_viaje;
+
+IF OBJECT_ID ('fk_fact_cliente', 'F') IS NOT NULL
+	ALTER TABLE GGDP.Factura DROP CONSTRAINT fk_fact_cliente;
+
+IF OBJECT_ID ('fk_fxv_factura', 'F') IS NOT NULL
+	ALTER TABLE GGDP.FacturaPorViaje DROP CONSTRAINT fk_fxv_factura;
+IF OBJECT_ID ('fk_fxv_viaje') IS NOT NULL
+	ALTER TABLE GGDP.FacturaPorViaje DROP CONSTRAINT fk_fxv_viaje;
+
+IF OBJECT_ID ('fk_rxv_rendicion', 'F') IS NOT NULL
+	ALTER TABLE GGDP.RendicionPorViaje DROP CONSTRAINT fk_rxv_rendicion;
+IF OBJECT_ID ('fk_rxv_viaje', 'F') IS NOT NULL
+	ALTER TABLE GGDP.RendicionPorViaje DROP CONSTRAINT fk_rxv_viaje;
+
+IF OBJECT_ID ('fk_rxu_rol', 'F') IS NOT NULL
+	ALTER TABLE GGDP.RolPorUsuario DROP CONSTRAINT fk_rxu_rol;
+
+IF OBJECT_ID ('fk_rxf_rol', 'F') IS NOT NULL
+	ALTER TABLE GGDP.RolPorFuncionalidad DROP CONSTRAINT fk_rxf_rol;
+IF OBJECT_ID ('fk_rxf_funcionalidad', 'F') IS NOT NULL
+	ALTER TABLE GGDP.RolPorFuncionalidad DROP CONSTRAINT fk_rxf_funcionalidad;
+GO
+*/
+
 /* Eliminacion de Tablas */
+IF OBJECT_ID('GGDP.FacturaPorViaje') IS NOT NULL
+    DROP TABLE GGDP.FacturaPorViaje;
+IF OBJECT_ID('GGDP.RendicionPorViaje') IS NOT NULL
+    DROP TABLE GGDP.RendicionPorViaje;
+IF OBJECT_ID('GGDP.RolPorUsuario') IS NOT NULL
+    DROP TABLE GGDP.RolPorUsuario;
+IF OBJECT_ID('GGDP.RolPorFuncionalidad') IS NOT NULL
+    DROP TABLE GGDP.RolPorFuncionalidad;
+IF OBJECT_ID('GGDP.Rendicion') IS NOT NULL
+    DROP TABLE GGDP.Rendicion;
+IF OBJECT_ID('GGDP.Viaje') IS NOT NULL
+    DROP TABLE GGDP.Viaje;
+IF OBJECT_ID('GGDP.Turno') IS NOT NULL
+    DROP TABLE GGDP.Turno;
+IF OBJECT_ID('GGDP.Factura') IS NOT NULL
+    DROP TABLE GGDP.Factura;
 IF OBJECT_ID('GGDP.Cliente') IS NOT NULL
     DROP TABLE GGDP.Cliente;
 IF OBJECT_ID('GGDP.Automovil') IS NOT NULL
@@ -39,15 +99,10 @@ IF OBJECT_ID('GGDP.Marca') IS NOT NULL
     DROP TABLE GGDP.Marca;
 IF OBJECT_ID('GGDP.Chofer') IS NOT NULL
     DROP TABLE GGDP.Chofer;
-
-IF OBJECT_ID('GGDP.Viaje') IS NOT NULL
-    DROP TABLE GGDP.Viaje;
-IF OBJECT_ID('GGDP.Rendicion') IS NOT NULL
-    DROP TABLE GGDP.Rendicion;
-IF OBJECT_ID('GGDP.Turno') IS NOT NULL
-    DROP TABLE GGDP.Turno;
-IF OBJECT_ID('GGDP.Factura') IS NOT NULL
-    DROP TABLE GGDP.Factura;
+IF OBJECT_ID('GGDP.Funcionalidad') IS NOT NULL
+    DROP TABLE GGDP.Funcionalidad;
+IF OBJECT_ID('GGDP.Rol') IS NOT NULL
+    DROP TABLE GGDP.Rol;
 GO
 
 /* Eliminacion del Eschema Principal */
@@ -115,17 +170,15 @@ CREATE TABLE GGDP.Viaje(
 );
 
 CREATE TABLE GGDP.Rendicion(
-	rend_id int not null,
+	rend_id INT IDENTITY PRIMARY KEY NOT NULL,
 	rend_chofer int not null ,
 	rend_turno int not null,
 	rend_importe numeric(18,2) not null,
-	rend_viaje int not null,
-	CONSTRAINT rend_pk PRIMARY KEY (rend_id)
+	rend_viaje int not null
 );
 	
 CREATE TABLE GGDP.Turno (
     turn_id INT IDENTITY PRIMARY KEY NOT NULL,
-    tuno_viaje int not null,
 	turn_hora_inicio numeric(18,0) not null,
 	turn_hora_fin numeric(18,0) not null,
 	turn_descripcion varchar(255) not null,
@@ -134,50 +187,47 @@ CREATE TABLE GGDP.Turno (
 );
 
 CREATE TABLE GGDP.Factura (
-    fact_id int not null,
+    fact_id INT IDENTITY PRIMARY KEY NOT NULL,
 	fact_fecha_inicio datetime,
 	fact_fecha_fin datetime,
 	fact_cliente int,
 	fact_importe decimal (12,2),
-	fact_viajes_facturados numeric(18,0),
-	CONSTRAINT fact_pk PRIMARY KEY (fact_id)
+	fact_viajes_facturados numeric(18,0)
 );
 
 
-CREATE TABLE GDD.FacturaPorViaje (
-    fxv_factura INT IDENTITY PRIMARY KEY NOT NULL,
-    fxv_viaje INT IDENTITY PRIMARY KEY NOT NULL,
+CREATE TABLE GGDP.FacturaPorViaje (
+    fxv_factura INT NOT NULL,
+    fxv_viaje INT NOT NULL,
 );
 
-CREATE TABLE GDD.RendicionPorViaje (
-    rxv_rendicion INT IDENTITY PRIMARY KEY NOT NULL,
-    rxv_viaje INT IDENTITY PRIMARY KEY NOT NULL,
+CREATE TABLE GGDP.RendicionPorViaje (
+    rxv_rendicion INT NOT NULL,
+    rxv_viaje INT NOT NULL,
 );
 
-CREATE TABLE GDD.Rol (
-	rol INT IDENTITY PRIMARY KEY NOT NULL,
+CREATE TABLE GGDP.Rol (
+	rol_id INT IDENTITY PRIMARY KEY NOT NULL,
 	rol_nombre VARCHAR(255) NOT NULL,
 	rol_habilitado BIT NOT NULL,
 );
 
-CREATE TABLE GDD.RolPorUsuario (
-	rxu_rol INT IDENTITY PRIMARY KEY NOT NULL, 
-	rxu_usuario INT IDENTITY PRIMARY KEY NOT NULL,
+CREATE TABLE GGDP.RolPorUsuario (
+	rxu_rol INT NOT NULL, 
+	rxu_usuario INT NOT NULL,
 );
 
-CREATE TABLE RolPorFuncionalidad (
-	rxf_rol INT IDENTITY PRIMARY KEY NOT NULL,
-	rxf_funcionalidad INT IDENTITY PRIMARY KEY,
+CREATE TABLE GGDP.RolPorFuncionalidad (
+	rxf_rol INT NOT NULL,
+	rxf_funcionalidad INT NOT NULL
 );
 
-CREATE TABLE Funcionalidad (
+CREATE TABLE GGDP.Funcionalidad (
 	func_id INT IDENTITY PRIMARY KEY NOT NULL,
 	func_nombre VARCHAR(255) NOT NULL,
 );
 
 GO
-
-
 
 /* Creacion de los FK */
 /* TODO FALTA LA TABLA CLIENTE
@@ -186,11 +236,10 @@ ALTER TABLE GGDP.Cliente
 	FOREIGN KEY (usua_id) REFERENCES GGDP.Usuario(usua_id)
 */
 ALTER TABLE GGDP.Automovil
-	ADD CONSTRAINT fk_auto_chofer
-	FOREIGN KEY (auto_chofer) REFERENCES GGDP.Chofer(chof_id)
+	ADD CONSTRAINT fk_auto_chofer FOREIGN KEY (auto_chofer) REFERENCES GGDP.Chofer(chof_id)
 
 ALTER TABLE GGDP.Viaje
-	ADD CONSTRAINT fk_chofer FOREIGN KEY (viaj_chofer) REFERENCES GGDP.Chofer(chof_id)
+	ADD CONSTRAINT fk_viaj_chofer FOREIGN KEY (viaj_chofer) REFERENCES GGDP.Chofer(chof_id)
 ALTER TABLE GGDP.Viaje
     ADD	CONSTRAINT fk_viaj_automovil FOREIGN KEY (viaj_automovil) REFERENCES GGDP.Automovil(auto_id)
 ALTER TABLE GGDP.Viaje
@@ -204,26 +253,35 @@ ALTER TABLE GGDP.Rendicion
 	ADD CONSTRAINT fk_rend_turno FOREIGN KEY (rend_turno) REFERENCES GGDP.Turno(turn_id)
 ALTER TABLE GGDP.Rendicion
 	ADD CONSTRAINT fk_rend_viaje FOREIGN KEY (rend_viaje) REFERENCES GGDP.Viaje(viaj_id)
+
 ALTER TABLE GGDP.Factura
 	ADD CONSTRAINT fk_fact_cliente FOREIGN KEY (fact_cliente) REFERENCES GGDP.Cliente(clie_id)
 	
-	
 ALTER TABLE GGDP.FacturaPorViaje
-	ADD CONSTRAINT fk_fxv_factura
-	FOREIGN KEY (fxv_factura) REFERENCES GGDP.Factura(fact_id)
-	
+	ADD CONSTRAINT fk_fxv_factura FOREIGN KEY (fxv_factura) REFERENCES GGDP.Factura(fact_id)
 ALTER TABLE GGDP.FacturaPorViaje
-	ADD CONSTRAINT fk_fxv_viaje
-	FOREIGN KEY (fxv_viaje) REFERENCES GGDP.Viaje(viaj_id)
+	ADD CONSTRAINT fk_fxv_viaje FOREIGN KEY (fxv_viaje) REFERENCES GGDP.Viaje(viaj_id)
 	
 ALTER TABLE GGDP.RendicionPorViaje
-	ADD CONSTRAINT fk_rxv_rendicion
-	FOREIGN KEY (rxv_rendicion) REFERENCES GGDP.Rendicion(rend_id)
-	
+	ADD CONSTRAINT fk_rxv_rendicion FOREIGN KEY (rxv_rendicion) REFERENCES GGDP.Rendicion(rend_id)
 ALTER TABLE GGDP.RendicionPorViaje
-	ADD CONSTRAINT fk_rxv_viaje
-	FOREIGN KEY (rxv_viaje) REFERENCES GGDP.Viaje(viaj_id)
+	ADD CONSTRAINT fk_rxv_viaje FOREIGN KEY (rxv_viaje) REFERENCES GGDP.Viaje(viaj_id)
 
+ALTER TABLE GGDP.RolPorUsuario
+	ADD CONSTRAINT fk_rxu_rol FOREIGN KEY (rxu_rol) REFERENCES GGDP.Rol(rol_id)
+
+/* TODO Falta tabla GGDP.Usuario */
+/*
+ALTER TABLE GGDP.RolPorUsuario
+	ADD CONSTRAINT fk_rxu_usuario
+	FOREIGN KEY (rxu_usuario) REFERENCES GGDP.Usuario(usuario_id)
+*/
+ALTER TABLE GGDP.RolPorFuncionalidad
+	ADD CONSTRAINT fk_rxf_rol
+	FOREIGN KEY (rxf_rol) REFERENCES GGDP.Rol(rol_id)
+ALTER TABLE GGDP.RolPorFuncionalidad
+	ADD CONSTRAINT fk_rxf_funcionalidad
+	FOREIGN KEY (rxf_funcionalidad) REFERENCES GGDP.Funcionalidad(func_id)
 /*
 ALTER TABLE GGDP.Chofer
 	ADD CONSTRAINT fk_chof_usuario
@@ -242,6 +300,7 @@ INSERT INTO GGDP.Cliente(clie_dni, clie_nombre, clie_apellido, clie_telefono, cl
 SELECT DISTINCT(Cliente_Dni), Cliente_Nombre, Cliente_Apellido, Cliente_Telefono, Cliente_Direccion, 1, Cliente_Mail, Cliente_Fecha_Nac, 1, 1 FROM [gd_esquema].[Maestra]
 /* TODO FALTA EL FK REAL DE CLIE_USUARIO */
 GO
+
 
 INSERT INTO GGDP.Chofer(chof_mail, chof_nombre, chof_apellido, chof_dni, chof_telefono, chof_direccion, chof_codigo_postal, chof_fecha_nacimiento, chof_habilitado, chof_usuario)
 SELECT DISTINCT (Chofer_Mail), Chofer_Nombre, Chofer_Apellido, Chofer_Dni, Chofer_Telefono, Chofer_Direccion, 1, Chofer_Fecha_Nac, 1, 1 FROM [gd_esquema].[Maestra]
