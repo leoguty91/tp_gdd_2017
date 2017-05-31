86a85d58e1e3b93bd08803a83c77d7ec65b6defd
leoguty91 <leoguty91@gmail.com>
2017-05-31 14:30:58 -0300

    Formulario basico
    Se agrega un formulario de base en chofer

--- TP/src/Abm Chofer/Form1.Designer.cs
+++ TP/src/Abm Chofer/Form1.Designer.cs
@@ -28,11 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_nombre = new System.Windows.Forms.TextBox();
+            this.bt_crear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // tb_nombre
+            // 
+            this.tb_nombre.Location = new System.Drawing.Point(67, 33);
+            this.tb_nombre.MaxLength = 255;
+            this.tb_nombre.Name = "tb_nombre";
+            this.tb_nombre.Size = new System.Drawing.Size(100, 20);
+            this.tb_nombre.TabIndex = 1;
+            // 
+            // bt_crear
+            // 
+            this.bt_crear.Location = new System.Drawing.Point(99, 202);
+            this.bt_crear.Name = "bt_crear";
+            this.bt_crear.Size = new System.Drawing.Size(75, 23);
+            this.bt_crear.TabIndex = 2;
+            this.bt_crear.Text = "Crear";
+            this.bt_crear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tb_nombre);
+            this.groupBox1.Location = new System.Drawing.Point(16, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bt_crear);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_nombre;
+        private System.Windows.Forms.Button bt_crear;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
