6397302edde80c82d86952531e193e5c03c3a8fd
leoguty91 <leoguty91@gmail.com>
2017-06-01 00:53:17 -0300

    Metodo conexion
    Se agrega el metodo conexion a la bd

--- TP/src/Helpers/Conexion.cs
+++ TP/src/Helpers/Conexion.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace UberFrba.Helpers
 {
     class Conexion
     {
+        public SqlConnection Connector { get; set; }
+        public SqlCommand Command { get; set; }
+
+        public Conexion()
+        {
+            var connection_string = ConfigurationManager.ConnectionStrings["UberFrba"].ConnectionString;
+            Connector = new SqlConnection(connection_string);
+        }
     }
 }
