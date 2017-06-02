849f11c9308dabb9313e4d22af066bf5d0863180
leoguty91 <leoguty91@gmail.com>
2017-06-02 14:45:19 -0300

    Funcion EjecutarConsulta e IniciarStoreProcedure
    Se agregan las funciones del helper conexion para manejar las consultas
    a la BD

--- TP/src/Helpers/Conexion.cs
+++ TP/src/Helpers/Conexion.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Windows.Forms;
 
 namespace UberFrba.Helpers
 {
@@ -17,6 +19,34 @@ namespace UberFrba.Helpers
         {
             var connection_string = ConfigurationManager.ConnectionStrings["UberFrba"].ConnectionString;
             Connector = new SqlConnection(connection_string);
+        }
+
+        public DataTable EjecutarConsultar(SqlCommand cmd)
+        {
+            try
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    Connector.Open();
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    Connector.Close();
+                    return dt;
+                }
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Error al ejecutar la consulta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public SqlCommand IniciarStoreProcedure(string nombre_sp)
+        {
+            string nombre_sp_completo = "GGDP." + nombre_sp;
+            SqlCommand cmd = new SqlCommand(nombre_sp_completo, Connector);
+            cmd.CommandType = CommandType.StoredProcedure;
+            return cmd;
         }
     }
 }
