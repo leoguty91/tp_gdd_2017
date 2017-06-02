aee9a0ea18c655d11ebfa81bb0bb562a3becd04b
leoguty91 <leoguty91@gmail.com>
2017-06-02 00:19:25 -0300

    Formulario login
    Se agrega el formulario de login al inicio

--- TP/src/Form1.Designer.cs
+++ TP/src/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usua_usuario = new System.Windows.Forms.TextBox();
+            this.usua_password = new System.Windows.Forms.TextBox();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_registrar);
+            this.groupBox1.Controls.Add(this.btn_ingresar);
+            this.groupBox1.Controls.Add(this.usua_password);
+            this.groupBox1.Controls.Add(this.usua_usuario);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 240);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loging";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // usua_usuario
+            // 
+            this.usua_usuario.Location = new System.Drawing.Point(28, 65);
+            this.usua_usuario.Name = "usua_usuario";
+            this.usua_usuario.Size = new System.Drawing.Size(204, 26);
+            this.usua_usuario.TabIndex = 2;
+            // 
+            // usua_password
+            // 
+            this.usua_password.Location = new System.Drawing.Point(28, 129);
+            this.usua_password.Name = "usua_password";
+            this.usua_password.Size = new System.Drawing.Size(204, 26);
+            this.usua_password.TabIndex = 3;
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(28, 174);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(94, 38);
+            this.btn_ingresar.TabIndex = 4;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(143, 174);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(89, 38);
+            this.btn_registrar.TabIndex = 5;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(281, 264);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Uber GGDP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.TextBox usua_password;
+        private System.Windows.Forms.TextBox usua_usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
